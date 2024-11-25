45c436e71f034255d4595d8c50c54f7930092dbb
zhuzedan <1031155817@qq.com>
2024-11-25 08:03:57 +0800

    feat:eslint配置

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import pluginJs from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error'
+    }
+  }
+]
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div>
     <h1>我是APP</h1>
   </div>
 </template>
-
+<script setup lang="ts">
+</script>
 <style scoped>
 .logo {
   height: 6em;
