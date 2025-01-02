ea0b70b9cdc8706ed01c8b20e7ad9ab674701dd8
zhuzedan <1031155817@qq.com>
2025-01-03 06:22:45 +0800

    feat:prettier配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,95 @@
-import pluginJs from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
+// eslint.config.js
+import eslint from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import eslintPluginVue from 'eslint-plugin-vue'
+import stylistic from '@stylistic/eslint-plugin'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+export default tseslint.config(
   {
+    ignores: ['node_modules', 'dist', 'public'],
+  },
+
+  /** js推荐配置 */
+  eslint.configs.recommended,
+  /** ts推荐配置 */
+  ...tseslint.configs.recommended,
+  /** vue推荐配置 */
+  ...eslintPluginVue.configs['flat/recommended'],
+
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+    jsx: true,
+    braceStyle: '1tbs',
+    arrowParens: 'always',
+  }),
+
+  /**
+   * javascript 规则
+   */
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
     rules: {
-      'no-unused-vars': 'error',
-      'no-undef': 'error'
-    }
-  }
-]
+      'no-console': 'warn',
+    },
+  },
+
+  /**
+   * 配置全局变量
+   */
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+
+        /** 追加一些其他自定义全局规则 */
+        wx: true,
+      },
+    },
+  },
+
+  /**
+   * vue 规则
+   */
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        /** typescript项目需要用到这个 */
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        /** 允许在.vue 文件中使用 JSX */
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      // 在这里追加 vue 规则
+      'vue/no-mutating-props': [
+        'error',
+        {
+          shallowOnly: true,
+        },
+      ],
+    },
+  },
+
+  /**
+   * typescript 规则
+   */
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    rules: {},
+  },
+
+  /**
+   * prettier 配置
+   * 会合并根目录下的.prettier.config.js 文件
+   * @see https://prettier.io/docs/en/options
+   */
+  eslintPluginPrettierRecommended,
+)
---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+export default {
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: false,
+  printWidth: 120,
+  arrowParens: 'always',
+  proseWrap: 'always',
+  endOfLine: 'lf',
+  experimentalTernaries: false,
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  vueIndentScriptAndStyle: false,
+  singleAttributePerLine: false,
+  jsxBracketSameLine: true
+}
